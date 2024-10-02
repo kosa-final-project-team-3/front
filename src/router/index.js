1dc9e98b6d98980e395d285c2121fa7a662815d0
이다영 <yooung513@gmail.com>
2024-10-02 15:11:01 +0800

    Update 파일 변경

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import AIFeedback from '../components/pages/AIFeedback.vue';
-import AIFeedback_2 from '../components/pages/AIFeedback_2.vue';
 
 const routes = [
     {
@@ -10,10 +9,6 @@ const routes = [
             {
                 path: '/feedback/ai',
                 component: AIFeedback,
-            },
-            {
-                path: '/feedback/ai2',
-                component: AIFeedback_2,
             },
         ],
     },
