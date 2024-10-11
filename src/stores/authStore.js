d9966e4a66d39fa905995367094a95201cd60fd7
havegrit <shin_jaean@naver.com>
2024-10-11 16:12:42 +0900

    refactor: 로그인 방식 개선 작업

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,22 +1,29 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import jwtAxios, { API_SERVER_HOST } from '../util/jwtUtil';
-import { getCookie } from '../util/cookieUtil';
-
-const host = API_SERVER_HOST;
+import axios from 'axios';
 
 export const useAuthStore = defineStore('auth', () => {
+    const host = API_SERVER_HOST;
     const isAuthenticated = ref(false);
+    const role = ref('');
+    const username = ref('');
 
     const checkAuthStatus = () => {
-        const id = getCookie('id');
-        const provider = getCookie('provider');
-
-        if (id && provider) {
-            isAuthenticated.value = true;
-        } else {
-            isAuthenticated.value = false;
-        }
+        axios
+            .get(`http://${host}/api/member/me`, { withCredentials: true })
+            .then((res) => {
+                if (res.status === 200) {
+                    const data = res.data;
+                    role.value = data.role;
+                    username.value = data.username;
+                    isAuthenticated.value = true;
+                }
+            })
+            .catch((e) => {
+                console.error(`authorization failed. \ncaused: ${JSON.stringify(e)}`);
+                isAuthenticated.value = false;
+            });
     };
 
     const logout = async () => {
@@ -33,6 +40,8 @@ export const useAuthStore = defineStore('auth', () => {
 
     return {
         isAuthenticated,
+        role,
+        username,
         checkAuthStatus,
         logout,
     };
