c6041b606131216b1df35fb49fa45200f30e7c0f
이다영 <yooung513@gmail.com>
2024-10-14 04:03:14 +0900

    Update OnlinePage

--- src/router/lessonpageRoutes.js
+++ src/router/lessonpageRoutes.js
@@ -1,9 +1,15 @@
 import OfflineLessonList from '../components/lesson/OfflineLessonList.vue';
+import OnlineLessonPage from '../pages/OnlineLessonPage.vue';
 
 export default [
     {
         path: '/lesson/offline',
         name: 'OfflineLessonList',
         component: OfflineLessonList,
+    },
+    {
+        path: '/lesson/online',
+        name: 'OnlineLessonPage',
+        component: OnlineLessonPage,
     },
 ];
