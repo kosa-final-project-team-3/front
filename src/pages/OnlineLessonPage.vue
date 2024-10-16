b2f98af169b5c0033465e4bebc39342d162222ce
이다영 <yooung513@gmail.com>
2024-10-17 06:52:41 +0900

    Update OnlineLessonPage.vue

--- src/pages/OnlineLessonPage.vue
+++ src/pages/OnlineLessonPage.vue
@@ -3,20 +3,33 @@
         <div class="content-wrapper">
             <aside class="sidebar">
                 <h3>온라인 레슨</h3>
-                <router-link class="sidebar-item" :to="{ name: 'OnlineLesson' }">온라인 PT</router-link>
-                <router-link class="sidebar-item" :to="{ name: 'OnlineFeedback' }">온라인 피드백</router-link>
+                <router-link
+                    class="sidebar-item"
+                    :to="{ name: 'OnlineLesson' }"
+                    :class="{ active: $route.name === 'OnlineLesson' }"
+                    >온라인 PT</router-link
+                >
+                <router-link
+                    class="sidebar-item"
+                    :to="{ name: 'OnlineFeedback' }"
+                    :class="{ active: $route.name === 'OnlineFeedback' }"
+                    >온라인 피드백</router-link
+                >
             </aside>
         </div>
 
         <div class="main-content">
             <router-view />
-            <MBTICompo />
+            <MBTICompo v-if="showMBTI" @close="showMBTI = false" />
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import MBTICompo from '../components/MBTI/MBTICompo.vue';
+
+const showMBTI = ref(true);
 </script>
 <style scoped>
 .container {
@@ -50,5 +63,45 @@ import MBTICompo from '../components/MBTI/MBTICompo.vue';
 
 .sidebar-item:hover {
     color: #f13223;
+}
+
+.sidebar-item.active {
+    color: #f13223;
+}
+
+.search-sort-container {
+    display: flex;
+    justify-content: space-between;
+    margin: 20px;
+}
+
+.search-container {
+    display: flex;
+}
+.search-container form {
+    display: flex;
+    gap: 10px;
+}
+
+.search-container button {
+    background-color: #f13223;
+    color: white;
+    border: 1px solid #f13223;
+}
+
+.sort-container select {
+    margin-right: 20px;
+}
+
+.search-container input {
+    width: 200px;
+}
+
+select,
+input,
+button {
+    padding: 5px 10px;
+    border-radius: 5px;
+    border: 1px solid #000000;
 }
 </style>
