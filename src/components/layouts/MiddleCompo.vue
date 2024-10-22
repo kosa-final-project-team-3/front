4a8e9047eb124b77dde67789f3ec02cc59f7b905
havegrit <79460763+havegrit@users.noreply.github.com>
2024-10-22 14:59:11 +0900

    [FEAT] 로그인 모달창 개선 (#90)
    * chore: add dummy and remove no need to be imported
    
    * refactor: 로그인 모달창 개선 작업
    
    close #87

--- src/components/layouts/MiddleCompo.vue
+++ src/components/layouts/MiddleCompo.vue
@@ -1,6 +1,22 @@
 <template>
-    <router-view> </router-view>
+    <router-view></router-view>
+    <Modal v-if="showLoginModal" @close="showLoginModal = false">
+        <KakaoLoginCompo />
+    </Modal>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import { useAuthStore } from '../../stores/authStore';
+import Modal from '../common/Modal.vue';
+import KakaoLoginCompo from '../login/KakaoLoginCompo.vue';
+
+const authStore = useAuthStore();
+const showLoginModal = ref(false);
+const openLogin = () => {
+    showLoginModal.value = true;
+};
+
+authStore.openLogin = openLogin;
+</script>
 <style scoped></style>
